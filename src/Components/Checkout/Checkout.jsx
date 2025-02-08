8480a2d67f9c8216be763976099503cc1be8fb91
AbdelrahmanDyaa <elnoby504@gmail.com>
2025-02-08 14:33:28 +0200

    update dependencies and modify checkout URL for production

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -16,7 +16,7 @@ export default function Checkout() {
   async function handleCheckout(shippingAddress) {
     try {
       setLoading(true);
-      const { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cart.cartId}?url=http://localhost:5173`, {
+      const { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cart.cartId}?url=https://abdelrahmandyaa.github.io/freshcart/#/`, {
        shippingAddress
        }
        ,
